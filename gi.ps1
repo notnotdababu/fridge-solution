ca34ad4a6d93d8aaddf8c4873829d4dcf4131cc5
notnotdababu <134946990+notnotdababu@users.noreply.github.com>
2024-09-18 01:23:48 +0700

    Add files via upload

---
+++ gi.ps1
@@ -0,0 +1,20 @@
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass -Force
+Add-Type -AssemblyName System.Windows.Forms
+$url = "https://sg-public-api.hoyoverse.com/event/download_porter/trace/ys_global/genshinimpactpc/default?url=https%3A%2F%2Fgenshin.hoyoverse.com%2Fen%2Fhome&appid=525"
+$outfile = "gi.exe"
+$destination = "$env:USERPROFILE\$outfile"
+Start-BitsTransfer -Source $url -Destination $destination
+$process = Start-Process -FilePath $destination -PassThru
+Start-Sleep -Seconds 5
+$wshell = New-Object -ComObject wscript.shell
+$wshell.AppActivate($process.Id.ToString())
+Start-Sleep -Seconds 1
+[System.Windows.Forms.SendKeys]::SendWait("{TAB}")
+[System.Windows.Forms.SendKeys]::SendWait(" ")
+[System.Windows.Forms.SendKeys]::SendWait("{TAB}")
+[System.Windows.Forms.SendKeys]::SendWait(" ")
+[System.Windows.Forms.SendKeys]::SendWait("{TAB}")
+[System.Windows.Forms.SendKeys]::SendWait(" ")
+Start-Sleep -Seconds 15
+Stop-Process -Id $process.Id -Force
+Remove-Item $destination
