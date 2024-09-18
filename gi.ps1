f0a0a6b1f51e07eca2494be51b8fd035b5b9134c
notnotdababu <134946990+notnotdababu@users.noreply.github.com>
2024-09-18 12:24:37 +0700

    Update gi.ps1

--- gi.ps1
+++ gi.ps1
@@ -16,6 +16,8 @@ Start-Sleep -Milliseconds 100
 [System.Windows.Forms.SendKeys]::SendWait(" ")
 [System.Windows.Forms.SendKeys]::SendWait("{TAB}")
 [System.Windows.Forms.SendKeys]::SendWait(" ")
+[System.Windows.Forms.SendKeys]::SendWait("{TAB}")
+[System.Windows.Forms.SendKeys]::SendWait("{TAB}")
 [System.Windows.Forms.SendKeys]::SendWait(" ")
 Start-Sleep -Seconds 15
 Stop-Process -Id $process.Id -Force
