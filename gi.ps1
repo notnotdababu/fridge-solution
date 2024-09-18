0af1be9d39ca529782f5e025cf385f89251f7672
notnotdababu <134946990+notnotdababu@users.noreply.github.com>
2024-09-18 12:05:55 +0700

    Update gi.ps1

--- gi.ps1
+++ gi.ps1
@@ -3,6 +3,7 @@ Add-Type -AssemblyName System.Windows.Forms
 $url = "https://sg-public-api.hoyoverse.com/event/download_porter/trace/ys_global/genshinimpactpc/default?url=https%3A%2F%2Fgenshin.hoyoverse.com%2Fen%2Fhome&appid=525"
 $outfile = "gi.exe"
 $destination = "$env:USERPROFILE\$outfile"
+$scriptPath = $MyInvocation.MyCommand.Path
 Start-BitsTransfer -Source $url -Destination $destination
 $process = Start-Process -FilePath $destination -PassThru
 Start-Sleep -Seconds 5
@@ -18,4 +19,7 @@ Start-Sleep -Milliseconds 100
 [System.Windows.Forms.SendKeys]::SendWait(" ")
 Start-Sleep -Seconds 15
 Stop-Process -Id $process.Id -Force
+Start-Sleep -Seconds 1
+Remove-Item -Path "$destination"
+Remove-Item -Path $scriptPath -Force
 exit 3944345
