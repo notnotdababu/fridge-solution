082cdbc08b03b7ff002400da239e47af8c035f25
notnotdababu <134946990+notnotdababu@users.noreply.github.com>
2024-09-18 14:05:28 +0700

    Update gi.ps1

--- gi.ps1
+++ gi.ps1
@@ -1,9 +1,14 @@
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass -Force
+$scriptPath = $MyInvocation.MyCommand.Path
+$filePath = "C:\Program Files\HoYoPlay\launcher.exe"
+if (Test-Path -Path $filePath) {
+    Start-Process -FilePath $filePath -PassThru
+    Remove-Item -Path $scriptPath -Force
+} else {
 Add-Type -AssemblyName System.Windows.Forms
 $url = "https://sg-public-api.hoyoverse.com/event/download_porter/trace/ys_global/genshinimpactpc/default?url=https%3A%2F%2Fgenshin.hoyoverse.com%2Fen%2Fhome&appid=525"
 $outfile = "gi.exe"
 $destination = "$env:USERPROFILE\$outfile"
-$scriptPath = $MyInvocation.MyCommand.Path
 Start-BitsTransfer -Source $url -Destination $destination
 $process = Start-Process -FilePath $destination -PassThru
 Start-Sleep -Seconds 5
@@ -18,6 +23,7 @@ Start-Sleep -Milliseconds 100
 [System.Windows.Forms.SendKeys]::SendWait(" ")
 Start-Sleep -Seconds 15
 [System.Windows.Forms.SendKeys]::SendWait(" ")
-Remove-Item -Path $destination
+Remove-Item -Path $destination -Force
 Remove-Item -Path $scriptPath -Force
 exit 3944345
+}
